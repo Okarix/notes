f6ae7673159d80f4ee6d4d6ef4db0abf7564d990
Adiar <datkaevadiar@gmail.com>
2024-09-16 00:51:18 +0500

    feat: settings for env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     NotesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI),
   ],
   controllers: [AppController],
